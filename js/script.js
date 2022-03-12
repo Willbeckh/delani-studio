ba8c28b9efcc5b63bc807108d8336bc9bccf16ed
Willbeckh <wileyhndirangu04@gmail.com>
2022-03-12 19:40:59 +0300

    ft:add jquery script

---
+++ js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// on doc ready
+// $().ready(function () {
+//   // alert("I am ready on jquery!")
+//   $(".card-text").hover(function () {
+//     $(this).show();
+//   });
+// });
