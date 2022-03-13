ce93af2644a071a533d139b8dbb05333a25b6ea4
Willbeckh <wileyhndirangu04@gmail.com>
2022-03-13 23:17:33 +0300

    ft: card-toggle-- create services cards toggle effect

--- js/script.js
+++ js/script.js
@@ -4,8 +4,25 @@
 $().ready(function () {
   console.log("page loaded.");
 
+  // switch what we do cards-description
+  $(".card1").click(() => {
+    $(".card-heading1").toggle();
+
+    $(".card1-text").toggle();
+
+  });$(".card2").click(() => {
+    $(".card-heading2").toggle();
+
+    $(".card2-text").toggle();
+  });$(".card3").click(() => {
+    $(".card-heading3").toggle();
+
+    $(".card3-text").toggle();
+  });
+
+
   // button event
-  $("button").click(()=> {
-    alert("Hello there, your message has been received😃")
-  })
+  $("button").click(() => {
+    alert("Hello there, your message has been received😃");
+  });
 });
