efb359d46cdfe2f4223a43c3fe7835b912973069
Willbeckh <wileyhndirangu04@gmail.com>
2022-03-15 00:26:31 +0300

    bg: remove typo on form class selector

--- js/script.js
+++ js/script.js
@@ -4,26 +4,6 @@
 $().ready(function () {
   console.log("page loaded.");
 
-<<<<<<< HEAD
-  // switch what we do cards-description
-  $(".card1").click(() => {
-    $(".card-heading1").toggle("slow");
-
-    $(".card1-text").toggle("slow");
-  });
-  $(".card2").click(() => {
-    $(".card-heading2").toggle("slow");
-
-    $(".card2-text").toggle("slow");
-  });
-  $(".card3").click(() => {
-    $(".card-heading3").toggle("slow");
-
-    $(".card3-text").toggle("slow");
-  });
-
-=======
->>>>>>> ft-form-validation
   // portfolio hover state
   $(".portfolio-card").hover(
     function () {
